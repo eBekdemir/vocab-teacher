832d508a6ff9bd073fb789ac84d9de440f000693
enes <42enesbekdemir@gmail.com>
2025-04-26 02:26:20 +0300

    initialize the ai and complete basic functions of theBot

--- theBot.py
+++ theBot.py
@@ -13,6 +13,8 @@ import sqlite3
 import threading
 import urllib3
 import pandas as pd
+from datetime import datetime, timedelta, timezone
+
 
 load_dotenv()
 os.environ['PYTHONIOENCODING'] = 'utf-8'
@@ -56,11 +58,11 @@ def init_db():
                         first_name TEXT,
                         last_name TEXT,
                         username TEXT,
-                        first_reminder INTEGER DEFAULT 1,
-                        second_reminder INTEGER DEFAULT 2,
-                        third_reminder INTEGER DEFAULT 4,
-                        fourth_reminder INTEGER DEFAULT 8,
-                        fifth_reminder INTEGER DEFAULT 16
+                        first_reminder INTEGER DEFAULT 0,
+                        second_reminder INTEGER DEFAULT 1,
+                        third_reminder INTEGER DEFAULT 3,
+                        fourth_reminder INTEGER DEFAULT 6,
+                        fifth_reminder INTEGER DEFAULT 14
                         )''')
         c.execute('''CREATE TABLE IF NOT EXISTS words (
                         id INTEGER PRIMARY KEY,
@@ -75,7 +77,8 @@ def init_db():
                         word_id INTEGER,
                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                         FOREIGN KEY (chat_id) REFERENCES chat_ids (chat_id),
-                        FOREIGN KEY (word_id) REFERENCES words (id)
+                        FOREIGN KEY (word_id) REFERENCES words (id),
+                        UNIQUE (chat_id, word_id) ON CONFLICT REPLACE
                         )''')
         c.execute('''CREATE TABLE IF NOT EXISTS messages (
                             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -127,37 +130,12 @@ def get_word_from_db(wrd: str) -> tuple[list[str], list[str]]:
         logger.error(f"Error retrieving a word ({wrd}): {e}")
         return None, None, None
 
-def get_user_words_from_db(chat_id: int) -> list[str]:
-    try:
-        with db_lock:
-            conn = sqlite3.connect(db_path)
-            cursor = conn.cursor()
-            cursor.execute('''SELECT word FROM words w
-                            JOIN user_words uw ON w.id = uw.word_id
-                            WHERE uw.chat_id = ?''', (chat_id,))
-            result = cursor.fetchall()
-            conn.close()
-        if result:
-            words = [row[0] for row in result]
-            logger.info(f"Retrieved words for chat ID {chat_id} from database.")
-            return words
-        else:
-            logger.warning(f"No words found for chat ID {chat_id}.")
-            return []
-    except sqlite3.Error as e: 
-        logger.error(f"SQLite error: {e}")
-        return []
-    except Exception as e:
-        logger.error(f"Error retrieving user words ({chat_id}): {e}")
-        return []
 
 def match_user_with_word(chat_id: int, word_id: int) -> bool: 
     try:
         with db_lock:
             conn = sqlite3.connect(db_path)
             cursor = conn.cursor()
-            # BUG: Check whether it throws an error or not if the word is already exist in db
-            # BUG: If the word is already matched with user, change its created date!!!
             cursor.execute('''INSERT INTO user_words (chat_id, word_id) VALUES (?, ?)''', (chat_id, word_id)) 
             conn.commit()
             conn.close()
@@ -236,13 +214,44 @@ def get_chat_ids_and_reminder_cycles() -> pd.DataFrame:
             logger.error(f"Database error getting chat IDs: {e}")
             return pd.DataFrame()
 
+def get_reminder_cycle_of_a_user(chat_id: int) -> tuple[int, int, int, int, int]:
+    with db_lock:
+        try:
+            with sqlite3.connect(db_path) as conn:
+                c = conn.cursor()
+                c.execute("""SELECT first_reminder, second_reminder, third_reminder, fourth_reminder, fifth_reminder FROM chat_ids WHERE chat_id = ?""", (chat_id,))
+                result = c.fetchone()
+                if result:
+                    logger.info(f"Retrieved reminder cycle for chat ID {chat_id}.")
+                    return result
+                else:
+                    logger.warning(f"No reminder cycle found for chat ID {chat_id}.")
+                    return None
+        except sqlite3.Error as e:
+            logger.error(f"Database error getting reminder cycle ({chat_id}): {e}")
+            return None
+        except Exception as e:
+            logger.error(f"Error while getting reminder cycle ({chat_id}): {e}")
+            return None
+
 def change_reminder_cycle_of_a_user(chat_id, first=1, second=2, third=4, fourth=8, fifth=16) -> bool:
     with db_lock:
         try:
             with sqlite3.connect(db_path) as conn:
                 c = conn.cursor()
-                # TODO: alter reminders
-                c.execute("""""")
+                c.execute("""
+                    UPDATE chat_ids
+                    SET first_reminder = ?,
+                        second_reminder = ?,
+                        third_reminder = ?,
+                        fourth_reminder = ?,
+                        fifth_reminder = ?
+                    WHERE chat_id = ?
+                """, (first, second, third, fourth, fifth, chat_id))
+                conn.commit()
+                if conn.total_changes == 0:
+                    logger.warning(f"No changes made to reminder cycle for chat ID {chat_id}.")
+                    return False                    
                 
                 logger.info(f"Altered {chat_id}'s reminder cycle.")
                 return True
@@ -270,6 +279,40 @@ def save_all_messages(chat_id, message):
             logger.error(f"Unexpected error saving message: {e}")
 
 
+def responsible_words(chat_id: int) -> list[str]:
+    now = datetime.now(timezone.utc)
+    reminders = get_reminder_cycle_of_a_user(chat_id)
+    if reminders is None:
+        logger.warning(f"No reminder cycle found for chat ID {chat_id}.")
+        return
+    time_params = [(now - timedelta(days=reminder)).date() for reminder in reminders]
+    placeholders = ', '.join(['?'] * len(time_params))
+    time_condition = f"AND DATE(uw.created_at) IN ({placeholders})"
+
+    with db_lock:
+        try:
+            with sqlite3.connect(db_path) as conn:
+                c = conn.cursor()
+                c.execute(f"""
+                    SELECT w.word FROM words w
+                    JOIN user_words uw ON w.id = uw.word_id
+                    WHERE uw.chat_id = ? {time_condition}
+                """, (chat_id, *time_params))
+                result = c.fetchall()
+                if result:
+                    words = [row[0] for row in result]
+                    logger.info(f"Retrieved responsible words for chat ID {chat_id}.")
+                    return words
+                else:
+                    logger.warning(f"No responsible words found for chat ID {chat_id}.")
+                    return []
+        except sqlite3.Error as e:
+            logger.error(f"Database error getting responsible words ({chat_id}): {e}")
+            return []
+        except Exception as e:
+            logger.error(f"Error while getting responsible words ({chat_id}): {e}")
+            return []
+
 
 
 
@@ -279,7 +322,8 @@ def escape_md(text):
     escape_chars = r"\_*[]()~`>#+-=|{}.!"
     return ''.join(f"\\{c}" if c in escape_chars else c for c in text)
 
-def handle_message(update: Update, context: CallbackContext):
+def handle_message(update: Update, context: CallbackContext): # TODO: Check whether the user exist or not!!!!
+    # FIXME: for poet: ERROR - Error in handle_message: Can't parse entities: character '.' is reserved and must be escaped with the preceding '\'
     chat_id = update.effective_chat.id
     message_text = update.message.text
 
@@ -318,6 +362,138 @@ def handle_message(update: Update, context: CallbackContext):
         update.message.reply_text("Something went wrong while processing your request.")
 
 
+def get_words_command(update: Update, context: CallbackContext) -> list[str]:
+    command = update.message.text.split()[0][1:] 
+    
+    if command == "words":
+        period = "all"
+    elif command == "today":
+        period = "today"
+    elif command == "this_week":
+        period = "this_week"
+    elif command == "responsibility":
+        period = "responsibility"
+    else:
+        update.message.reply_text("Invalid command. Please use /words, /today, or /this_week.")
+        return
+
+    chat_id = update.effective_chat.id
+
+    time_condition = ""
+    now = datetime.now(timezone.utc)
+
+    if period == "today":
+        if now.hour < 12:
+            start_of_day = now - timedelta(hours=18)
+        else:
+            start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        time_condition = "AND uw.created_at >= ?"
+        time_params = [start_of_day]
+    elif period == "this_week":
+        start_time = now - timedelta(days=7)
+        time_condition = "AND uw.created_at >= ?"
+        time_params = [start_time]
+    elif period == "responsibility":
+        reminders = get_reminder_cycle_of_a_user(chat_id)
+        if reminders is None:
+            update.message.reply_text("You don't have any reminders set.")
+            return
+        valid_dates = [(now - timedelta(days=reminder)).date() for reminder in reminders]
+        placeholders = ', '.join(['?'] * len(valid_dates))
+        time_condition = f"AND DATE(uw.created_at) IN ({placeholders})"
+        time_params = valid_dates
+    else:
+        time_params = []
+
+    try:
+        with db_lock:
+            conn = sqlite3.connect(db_path)
+            cursor = conn.cursor()
+            query = f'''
+                SELECT w.word FROM words w
+                JOIN user_words uw ON w.id = uw.word_id
+                WHERE uw.chat_id = ? {time_condition}
+            '''
+            cursor.execute(query, [chat_id] + time_params)
+            result = cursor.fetchall()
+            conn.close()
+
+        if result:
+            words = [row[0] for row in result]
+            logger.info(f"Retrieved {len(words)} words for chat ID {chat_id} (period: {period}).")
+        else:
+            logger.warning(f"No words found for chat ID {chat_id} (period: {period}).")
+            words = []
+    except sqlite3.Error as e: 
+        logger.error(f"SQLite error: {e}")
+        words = []
+    except Exception as e:
+        logger.error(f"Error retrieving user words by command ({chat_id}, {period}): {e}")
+        words = []
+
+    if words:
+        words_text = "\n".join(f"{i+1}. {word}" for i, word in enumerate(words))
+        dct = {
+            "all": "all time",
+            "today": "today",
+            "this_week": "this week",
+            "responsibility": "your responsibility dates"
+        }
+        update.message.reply_text(
+            f"Here are your words ({len(words)}) for {dct[period]}:\n\n{words_text}"
+        )
+    else:
+        update.message.reply_text(
+            f"You don't have any words in your vocabulary yet. "
+            f"Please send me a word to start learning!"
+        )
+    logger.info(f"Words command executed for chat ID {chat_id} (period: {period}).")
+
+
+def get_reminder_command(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
+    reminders = get_reminder_cycle_of_a_user(chat_id)
+    if reminders is None:
+        update.message.reply_text("You don't have any reminders set.")
+        return
+
+    reminder_text = (
+        f"Your reminder cycle:\n"
+        f"""1st reminder: {'Today' if reminders[0] == 0 else f"{reminders[0]} day{'s' if reminders[0]>1 else ''}"}\n"""
+        f"2nd reminder: {reminders[1]} day{'s' if reminders[1] > 1 else ''} ago\n"
+        f"3rd reminder: {reminders[2]} days ago\n"
+        f"4th reminder: {reminders[3]} days ago\n"
+        f"5th reminder: {reminders[4]} days ago\n"
+        f"\nYou can change your reminder cycle with /set_reminders command."
+    )
+    update.message.reply_text(reminder_text)
+    logger.info(f"Reminder command executed for chat ID {chat_id}.")
+
+def set_reminder_command(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
+    if len(context.args) != 5:
+        update.message.reply_text("Please provide 5 reminder intervals in days like the example:\n/set_reminders 1 2 4 8 16")
+        return
+
+    try:
+        reminders = [int(arg) for arg in context.args]
+        if any(reminder < 0 for reminder in reminders):
+            raise ValueError("Reminder intervals must be non-negative.")
+
+        if change_reminder_cycle_of_a_user(chat_id, *reminders):
+            update.message.reply_text(f"Your reminder cycle has been updated successfully.")
+            logger.info(f"Reminder cycle updated for chat ID {chat_id}.")
+        else:
+            update.message.reply_text("Failed to update your reminder cycle.")
+            logger.error(f"Failed to update reminder cycle for chat ID {chat_id}.")
+    except ValueError as e:
+        update.message.reply_text(f"Invalid input: {e}")
+        logger.error(f"Invalid input for reminder command ({chat_id}): {e}")
+    except Exception as e:
+        update.message.reply_text("An error occurred while updating your reminder cycle.")
+        logger.error(f"Error while updating reminder cycle ({chat_id}): {e}")
+
+
 def start(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     user_name = update.effective_user.first_name
@@ -344,13 +520,31 @@ def stop(update: Update, context: CallbackContext):
     if delete_chat_id(chat_id):
         update.message.reply_text("You have successfully stopped the bot. You can restart the bot with /start command. Good Bye! 👋")
     else:
-        update.message.reply_text(".") # TODO: add a text here
+        update.message.reply_text("You are not in our database. Please contact with the admin:\n\n- Kömen | 42enesbekdemir@gmail.com")
 
-# TODO: add a help command
 
-# TODO: add today's words command (the words that passed in last 24 hour)
-# TODO: add this week's words command
+def help_command(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "I am here to support your vocabulary level!\n"
+        "When you don't know a word, then just pass me the word!\n"
+        "I will give you the definitions and examples of the word.\n\n"
+        "You can also use the following commands:\n"
+        "/start - Start the bot\n"
+        "/stop - Stop the bot\n"
+        "/help - Show this help message\n"
+        "/words - Show your words\n"
+        "/today - Show today's added words\n"
+        "/this_week - Show this week's added words\n"
+        "/responsibility - Show the words that you are responsible today\n"
+        "/reminder - Show your reminder cycle\n"
+        "/set_reminders - Change your reminder cycle\n"
+        "\n- Kömen")
 
+def test(update: Update, context: CallbackContext): # TODO: remove this function
+    chat_id = update.effective_chat.id
+    user_name = update.effective_user.first_name
+    logger.info(f"/test command received from {user_name} ({chat_id})")
+    update.message.reply_text(responsible_words(chat_id))
 
 
 
@@ -368,8 +562,20 @@ def main() -> None:
     dispatcher = updater.dispatcher
     job_queue = updater.job_queue
 
+    dispatcher.add_handler(CommandHandler("test", test)) # TODO: remove this line
+
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("stop", stop))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    
+    dispatcher.add_handler(CommandHandler("words", get_words_command))
+    dispatcher.add_handler(CommandHandler("today", get_words_command))
+    dispatcher.add_handler(CommandHandler("this_week", get_words_command))
+    dispatcher.add_handler(CommandHandler("responsibility", get_words_command))
+    
+    dispatcher.add_handler(CommandHandler("reminder", get_reminder_command))
+    dispatcher.add_handler(CommandHandler("set_reminders", set_reminder_command))
+
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))    
     logger.info("Command handlers registered.")
 
